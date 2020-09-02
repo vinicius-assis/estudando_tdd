a7ab0fec5ac2f62d5c44d8855b8667f451c27b10
Vinicius <viniciusassis.dev@hotmail.com>
2020-09-02 18:59:31 -0300

    Create filter test

---
+++ filter/filter.test.js
@@ -0,0 +1,49 @@
+import { expect } from 'chai'
+import filter from './filter'
+
+it('filter should be a function', () => {
+  expect(filter).to.be.a('function')
+})
+
+it('filter([1, 2], (item) => item) should return [1, 2]', () => {
+  expect(filter([1, 2], (item) => item)).to.be.deep.equal([1, 2])
+})
+
+it('filter([4, 5], (item) => item) should return [4, 5]', () => {
+  expect(filter([4, 5], (item) => item)).to.be.deep.equal([4, 5])
+})
+
+it('filter([1, 2, 3], (item) => item > 1) should return [2, 3]', () => {
+  expect(filter([1, 2, 3], (item) => item > 1)).to.be.deep.equal([2, 3])
+})
+
+it('filter([4, 5, 6], (item) => item < 6) shoudl return [4, 5]', () => {
+  expect(filter([4, 5, 6], (item) => item < 6)).to.be.deep.equal([4, 5])
+})
+
+it('filter([]) should return []', () => {
+  expect(filter([])).to.be.deep.equal([])
+})
+
+//
+
+it('filter([1, 2, 3], (item) => item) should return [1, 2, 3]', () => {
+  expect(filter([1, 2, 3], (item) => item)).to.be.deep.equal([1, 2, 3])
+})
+
+it('filter([0, 1, 2], (item) => item) should return [1, 2]', () => {
+  expect(filter([0, 1, 2], (item) => item)).to.be.deep.equal([1, 2])
+})
+
+it('filter([1, 2, 3], (item) => item < 2) should return [1]', () => {
+  expect(filter([1, 2, 3], (item) => item < 2)).to.be.deep.equal([1])
+})
+
+it('filter([1, 2, 3, 5], (item, index) => item === index + 1) should return [1, 2, 3]', () => {
+  expect(filter([1, 2, 3, 5], (item, index) => item === index + 1)).to.be.deep.equal([1, 2, 3])
+})
+
+it('filter([1, 2, 3, 2, 1, 5], (item, index, array) => index === array.indexOf(item)) should return [1, 2, 3, 5]', () => {
+  const before = filter([1, 2, 3, 2, 1, 5], (item, index, array) => index === array.indexOf(item))
+  expect(before).to.be.deep.equal([1, 2, 3, 5])
+})
